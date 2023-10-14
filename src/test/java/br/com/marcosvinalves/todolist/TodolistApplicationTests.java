577848a805830bc80b6c17f158096f66f400426e
Marcos Vinicius <marcos_araujo7@hotmail.com>
2023-10-14 13:25:44 -0300

    Initial commit

---
+++ src/test/java/br/com/marcosvinalves/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosvinalves.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
